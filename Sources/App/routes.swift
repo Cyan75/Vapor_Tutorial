07b398e7e52bff7450efc3f9bbbc3e3f756ad4b6
Cyan75 <incanto075@gmail.com>
2021-08-18 00:18:51 +0900

    127.0.0.1:8080/hello

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,17 @@
 import Vapor
 
-func routes(_ app: Application) throws {
-    app.get { req in
+func routes(_ router: Application) throws {
+    router.get { req in
         return "It works!"
     }
 
-    app.get("hello") { req -> String in
-        return "Hello, world!"
+    router.get("hello") { req -> String in
+        return "Hello, Cyan!"
+    }
+    
+    router.get("greet", ":name"){ request -> String in
+        let name = request.parameters.get("name")!
+        return "Hello \(name)"
     }
 }
+ 
